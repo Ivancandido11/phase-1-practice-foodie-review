e76d3a45d8a2dd48c13cfcb90c6b13e3ffe5f4b3
Ivan Candido <ivan.candido@powerhrg.com>
2021-06-08 09:19:18 -0400

    Gets the first dish and all of its info  on the page once page loads

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// Code here
+const updateDishDescriptionBtn = document.querySelector("body > main > div > form.description > button")
+
+const getDish = (dish) => {
+  const dishName = document.querySelector("body > main > div > h2")
+  const dishImage = document.querySelector("body > main > div > img")
+  const dishDescription = document.querySelector("body > main > div > form.description > textarea")
+  const dishReviewsUl = document.querySelector("body > main > div > ul")
+  const existingReviews = document.querySelectorAll("body > main > div > ul > li")
+  existingReviews.forEach(eReview => {
+    dishReviewsUl.removeChild(eReview)
+  })
+  dishName.innerHTML = dish.name
+  dishImage.src = dish.image_url
+  dishDescription.innerHTML = dish.description
+  dish.reviews.forEach(review => {
+    const reviewList = document.createElement("li")
+    reviewList.innerHTML = review
+    dishReviewsUl.append(reviewList)
+  })
+}
+
+const init = () => {
+  fetch("http://localhost:3000/dishes/1")
+    .then(resp => resp.json())
+    .then(dish => {
+      getDish(dish)
+    })
+}
+
+document.addEventListener("DOMContentLoaded", init)
